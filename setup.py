96e3b735a431eaceec8128b930a3b266958c7c8c
potichek <bibilmeshka@gmail.com>
2024-07-09 15:35:15 +0300

    Version 0.0.9 bug fixes (fixing path to dll)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setuptools.setup(
     name='winfuncs',
-    version='0.0.8',
+    version='0.0.9',
     author='potichek',
     author_email='bibilmeshka@gmail.com',
     description='Simplifying various WinAPI functions, with this library you won`t need to use ctypes to call WinAPI functions.',
--- winfuncs/_winfuncs.py
+++ winfuncs/_winfuncs.py
@@ -1,8 +1,10 @@
 import ctypes
 import os
 
-print('path: ' + os.path.join(os.getcwd(), 'WinFuncs.dll'))
-_WinFuncs_dll = ctypes.CDLL(os.path.join(os.getcwd(), 'WinFuncs.dll'))
+path_dll = str(ctypes.__path__[0])
+path_dll = path_dll[0 : (len(path_dll) - 6)] + 'WinFuncs.dll'
+print(f'path: {path_dll}')
+_WinFuncs_dll = ctypes.CDLL(path_dll)
 _WinFuncs_dll.move_cursor_absolute.argtypes = (ctypes.c_short, ctypes.c_short)
 _WinFuncs_dll.move_cursor.argtypes = (ctypes.c_short, ctypes.c_short)
 
